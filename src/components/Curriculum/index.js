dc3daf75df98597683ee4067aafc9f84c079c9e8
GianDutra <chikegang@gmail.com>
2023-06-19 17:08:04 -0300

    Atualizando informacoes na parte curriculum

--- src/components/Curriculum/index.js
+++ src/components/Curriculum/index.js
@@ -11,16 +11,7 @@ const Curriculum = () => {
                 <h4>Engenharia de Software</h4>
                 <span class="casa">PUCCAMP</span>
                 <span class="fecha">2021 - 2024</span>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, cumque repellat, tempora recusandae aliquam nemo esse natus impedit, nostrum temporibus veritatis eaque soluta aperiam id repudiandae fugiat deserunt! Explicabo, veritatis?</p>
-                <div class="conectori">
-                    <div class="circuloi"></div>
-                </div>
-            </div>
-            <div class="item izq">
-                <h4>Ensino Médio</h4>
-                <span class="casa">Colégio Objetivo Itapira</span>
-                <span class="fecha">2018 - 2020</span>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, cumque repellat, tempora recusandae aliquam nemo esse natus impedit, nostrum temporibus veritatis eaque soluta aperiam id repudiandae fugiat deserunt! Explicabo, veritatis?</p>
+                <p>Aqui estou vivendo uma verdadeira imersão no mundo da criação tecnológica, tendo a oportunidade de desenvolver e aprimorar minhas skills como engenheiro de software, entendendo e participando em cada etapa do processo de construção de um software através dos projetos integradores e projetos pessoais. Esta jornada emocionante reforça cada vez mais a certeza de que fiz a escolha correta.</p>
                 <div class="conectori">
                     <div class="circuloi"></div>
                 </div>
@@ -29,7 +20,16 @@ const Curriculum = () => {
                 <h4>Curso de Inglês | Espanhol</h4>
                 <span class="casa">CNA</span>
                 <span class="fecha">2014 - 2020 | 2018 - 2020</span>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, cumque repellat, tempora recusandae aliquam nemo esse natus impedit, nostrum temporibus veritatis eaque soluta aperiam id repudiandae fugiat deserunt! Explicabo, veritatis?</p>
+                <p>Descobri no CNA o poder de desvendar o mundo através dos idiomas. Nesse curso de Inglês e Espanhol, mergulhei em um universo fascinante, onde aprender vai além das palavras. Cada aula me levou a compreender a cultura e a alma das pessoas que falam essas línguas, abrindo portas para conhecer pessoas maravilhosas, além de me fazer começar a estudar outros idiomas.</p>
+                <div class="conectori">
+                    <div class="circuloi"></div>
+                </div>
+            </div>
+            <div class="item izq">
+                <h4>Ensino Médio</h4>
+                <span class="casa">Colégio Objetivo Itapira</span>
+                <span class="fecha">2018 - 2020</span>
+                <p> Foi nessa etapa da minha vida que descobri minha paixão pela programação, desenvolvendo meu primeiro aplicativo com um toque de portonhol. Essa experiência única e encorajadora foi o impulso decisivo para trilhar uma carreira nesse campo fascinante.</p>
                 <div class="conectori">
                     <div class="circuloi"></div>
                 </div>
@@ -42,16 +42,16 @@ const Curriculum = () => {
                 <h4>Business Inteligence</h4>
                 <span class="casa">PUCCAMP - DPaschoal</span>
                 <span class="fecha">2023 - Atualmente</span>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, cumque repellat, tempora recusandae aliquam nemo esse natus impedit, nostrum temporibus veritatis eaque soluta aperiam id repudiandae fugiat deserunt! Explicabo, veritatis?</p>
+                <p>Estou atualmente participando na implementação de diversos dashboards utilizando os conceitos de Business Intelligence, com o objetivo de fornecer à empresa uma compreensão profunda de seus clientes e outras informações relevantes. Esses painéis interativos estão sendo desenvolvidos utilizando as tecnologias Power BI, React.js, Express.js e SQL server.</p>
                 <div class="conectord">
                     <div class="circulod"></div>
                 </div>
             </div>
             <div class="item der">
-                <h4>Projeto Integrador IV</h4>
+                <h4>Aplicação Web</h4>
                 <span class="casa">Via Fácil</span>
                 <span class="fecha">2022 (2º Semestre)</span>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, cumque repellat, tempora recusandae aliquam nemo esse natus impedit, nostrum temporibus veritatis eaque soluta aperiam id repudiandae fugiat deserunt! Explicabo, veritatis?</p>
+                <p>Essa aplicação web revolucionou a forma como as pessoas emitiam suas segundas vias, proporcionando facilidade ao concentrar todas as opções em um único local. Com a utilização de tecnologias modernas como React.js, Spring Boot e MongoDB, o aplicativo oferece aos usuários opções de login tradicional e escaneamento de QR Code, ambos utilizando o JWT. </p>
                 <div class="conectord">
                     <div class="circulod"></div>
                 </div>
@@ -60,7 +60,7 @@ const Curriculum = () => {
                 <h4>Monitoria de Python</h4>
                 <span class="casa">PUCCAMP</span>
                 <span class="fecha">2022 (1º semestre)</span>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, cumque repellat, tempora recusandae aliquam nemo esse natus impedit, nostrum temporibus veritatis eaque soluta aperiam id repudiandae fugiat deserunt! Explicabo, veritatis?</p>
+                <p>Como monitor de Python, compartilhei conhecimentos e auxiliei estudantes no domínio dessa linguagem versátil e poderosa. Essa experiência enriquecedora fortaleceu minha própria compreensão de Python, enquanto apoiei outros na descoberta dos segredos dessa linguagem de programação. </p>
                 <div class="conectord">
                     <div class="circulod"></div>
                 </div>
