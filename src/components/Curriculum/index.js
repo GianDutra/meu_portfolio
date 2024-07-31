014cd58c423d281e7f1340149d525fd6653c4f10
GianDutra <chikegang@gmail.com>
2024-07-31 13:27:41 -0300

    fix: update Curriculum section with refined descriptions and mobile title adjustment

--- src/components/Curriculum/index.js
+++ src/components/Curriculum/index.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { useMediaQuery } from 'react-responsive';
 
 const Curriculum = () => {
   useEffect(() => {
     AOS.init();
   }, []);
+
+const isMobile = useMediaQuery({ query: '(max-width: 700px)' });
 
   return (
     <section id="curriculum" className="curriculum">
@@ -24,10 +27,28 @@ const Curriculum = () => {
               </div>
             </div>
             <div className="item izq" data-aos="fade-right" data-aos-duration="1500" data-aos-easing="ease">
+              <h4>React.js | Node.js Specialization</h4>
+              <span className="casa">Rocketseat</span>
+              <span className="fecha">2023-2024</span>
+              <p>Completed a comprehensive specialization in React.js and Node.js with Rocketseat. Acquired expertise in building dynamic front-end applications with React.js and crafting robust back-end solutions using Node.js. Gained hands-on experience with modern tools like Next.js, Docker, and Tailwind CSS, enhancing my full-stack development skills.</p>
+              <div className="conectori">
+                <div className="circuloi"></div>
+              </div>
+            </div>
+            <div className="item izq" data-aos="fade-right" data-aos-duration="1500" data-aos-easing="ease">
+              <h4>Lean Six Sigma Green Belt</h4>
+              <span className="casa">EDTI</span>
+              <span className="fecha">2024 (1st Semester)</span>
+              <p>During this certification, I explored Lean Six Sigma principles, enhancing my ability to analyze and improve processes. I completed a practical project using Minitab, which sharpened my statistical skills and helped me understand complex data. This experience solidified my commitment to process optimization and continuous improvement.</p>
+              <div className="conectori">
+                <div className="circuloi"></div>
+              </div>
+            </div>
+            <div className="item izq" data-aos="fade-right" data-aos-duration="1500" data-aos-easing="ease">
               <h4>English | Spanish Course</h4>
               <span className="casa">CNA</span>
               <span className="fecha">2014 - 2020 | 2018 - 2020</span>
-              <p>I discovered at CNA the power of unveiling the world through languages. On this course, I immersed myself in a fascinating universe where learning goes beyond words. Each class led me to understand the culture and soul of people who speak these languages, opening doors to meet wonderful people and inspiring me to start studying other languages.</p>
+              <p>I discovered at CNA how languages beautifully reveal the world. Immersed in this course, I explored far beyond mere words. Each class deepened my understanding of diverse cultures and connected me with amazing people, inspiring me to continue learning more languages.</p>
               <div className="conectori">
                 <div className="circuloi"></div>
               </div>
@@ -45,16 +66,25 @@ const Curriculum = () => {
           <div className="col derecha">
             <h3>Practical Experience</h3>
             <div className="item der" data-aos="fade-left" data-aos-duration="1500" data-aos-easing="ease">
+              <h4>{isMobile ? 'Intern in Digital Transformation' : 'Digital Transformation Intern'}</h4>
+              <span className="casa">Ypê</span>
+              <span className="fecha">2024 - Present</span>
+              <p>I am working on simplifying project management tools using Power BI, Excel, and PWA. My role involves exploring AI tools for project management, conducting meetings in English and Spanish, and automating workflows with Power Automate. Additionally, I create interactive dashboards to enhance project oversight and decision-making.</p>
+              <div className="conectord">
+                <div className="circulod"></div>
+              </div>
+            </div>
+            <div className="item der" data-aos="fade-left" data-aos-duration="1500" data-aos-easing="ease">
               <h4>Business Intelligence</h4>
               <span className="casa">PUCCAMP - DPaschoal</span>
-              <span className="fecha">2023 - Present</span>
+              <span className="fecha">2023 (Full Year)</span>
               <p>I am currently involved in the implementation of various dashboards using Business Intelligence concepts to provide the company with deep insights into its customers and other relevant information. These interactive dashboards are being developed using technologies such as Power BI, React.js, Express.js, and SQL Server.</p>
               <div className="conectord">
                 <div className="circulod"></div>
               </div>
             </div>
             <div className="item der" data-aos="fade-left" data-aos-duration="1500" data-aos-easing="ease">
-              <h4>Web Application</h4>
+              <h4>Simplified Access Hub</h4>
               <span className="casa">Via Fácil</span>
               <span className="fecha">2022 (2nd Semester)</span>
               <p>This web application revolutionized the way people obtained their second copies by providing convenience through a centralized platform. With the use of modern technologies like React.js, Spring Boot, and MongoDB, the application offered users options for traditional login and QR Code scanning, both utilizing JWT authentication.</p>
@@ -67,6 +97,15 @@ const Curriculum = () => {
               <span className="casa">PUCCAMP</span>
               <span className="fecha">2022 (1st Semester)</span>
               <p>As a Python tutor, I shared knowledge, guided, and assisted students in mastering this versatile, powerful, and widely-used language. This enriching experience strengthened my own understanding of Python while helping others unravel the secrets and intricacies of this dynamic programming language.</p>
+              <div className="conectord">
+                <div className="circulod"></div>
+              </div>
+            </div>
+            <div className="item der" data-aos="fade-left" data-aos-duration="1500" data-aos-easing="ease">
+              <h4>Digital Inclusion Volunteer</h4>
+              <span className="casa">Vitalità</span>
+              <span className="fecha">2022 (1st Semester)</span>
+              <p>I supported elderly individuals, aged 70 and above, through one-on-one training sessions to help them use recent technologies. This initiative aimed to significantly enhance their digital independence, boost their confidence, and improve their ability to navigate modern tools effectively.</p>
               <div className="conectord">
                 <div className="circulod"></div>
               </div>
